a7d257a43d798cab4fdb73ae7564a7d772ac0b24
Jonathan <jonathancbadger@gmail.com>
2021-03-30 08:09:09 -0500

    Initial Commit

---
+++ FormattedTextField/FormattedTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextFieldApp.swift
+//  FormattedTextField
+//
+//  Created by Jonathan Badger on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
